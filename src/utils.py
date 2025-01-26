78e47414cf43e805b4d01ec9dc1b096f6ab9fc8a
Jasjeev <jasjeev99@gmail.com>
2025-01-26 13:33:35 +0530

    Add model training component with evaluation and model persistence functionality

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,8 @@ import pandas as pd
 from src.exception import CustomException
 
 import dill
+from sklearn.metrics import r2_score
+from src.logger import logging
 
 def save_object(file_path,obj):
     try:
@@ -16,3 +18,18 @@ def save_object(file_path,obj):
             dill.dump(obj, file_obj)
     except Exception as e:
         raise CustomException("Error in Saving Object", e)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models: dict):
+    model_report = {}
+    for model_name, model in models.items():
+        try:
+            logging.info(f"Training model: {model_name}")
+            model.fit(X_train, y_train)
+            y_pred = model.predict(X_test)
+            r2 = r2_score(y_test, y_pred)
+            model_report[model_name] = r2
+            logging.info(f"Model: {model_name} trained successfully")
+        except Exception as e:
+            logging.error(f"Error while training model: {model_name}")
+            logging.error(e)
+    return model_report
